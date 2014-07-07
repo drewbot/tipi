bca29981320bcaee59fe5ab1b0d157664aeb54a0
Drew Botka <hellodrewbot@gmail.com>
2014-07-07 14:55:55 -0400

    Got new posts working and configured adapter

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,23 +7,26 @@ App = Ember.Application.create();
 //   host: 'http://tiny-pizza-server.herokuapp.com/collections/ember-practice'
 // });
 
-/////////// Attempting to change _id to id /////////
+// ///////// Attempting to change _id to id /////////
 
-// App.ApplicationSerializer = DS.RESTSerializer.extend({
-//   primaryKey: "_id"
-// });
-
-/////////// Attempting to change _id to id /////////
-App.Adapter = DS.RESTAdapter.extend({
-  host: 'http://tiny-pizza-server.herokuapp.com/collections/ember-practice',
-  serializer: DS.RESTSerializer.extend({
-    primaryKey: function (type){
-      return '_id';
-   }
-  })
+App.ApplicationSerializer = DS.RESTSerializer.extend({
+  primaryKey: "_id"
 });
 
-App.Store = DS.Store.extend({
+// App.adapter is being used below instead of App.ApplicationAdapter and 
+// App.ApplicationSerializer above. Not sure if it will effect adversely when
+// working correctly because both throw the same errors and don't do what they're supposed to
+
+/////////// Attempting to change _id to id /////////
+
+DS.RESTAdapter.reopen({
+  host: 'http://tiny-pizza-server.herokuapp.com',
+  namespace: 'ember-collections'
+});
+
+App.Adapter = DS.RESTAdapter;
+
+App.ApplicationStore = DS.Store.extend({
   revision: 11,
   adapter: 'App.Adapter'
 });
@@ -41,13 +44,19 @@ App.Router.map(function(){
 
 App.Route = Ember.Route.extend({
 	model: function(){
-		return $.getJSON('http://tiny-pizza-server.herokuapp.com/collections/ember-practice')
+		return $.getJSON('http://tiny-pizza-server.herokuapp.com/ember-collections/projects')
 	}
+	// model: function() {
+	//     return this.store.find('project'); // Error while loading route: Error: No model was found for '0'
+	// }
+	// model: function() {
+	//     return this.store.find('projects'); // Error while loading route: Error: No model was found for 'projects'
+	// }
 });
 
 App.DraftRoute = Ember.Route.extend({
 	model: function(params){
-		return $.getJSON('http://tiny-pizza-server.herokuapp.com/collections/ember-practice/'+params.draft_id+'')
+		return $.getJSON('http://tiny-pizza-server.herokuapp.com/ember-collections/projects/'+params.draft_id+'')
 	}
 });
 
@@ -56,10 +65,10 @@ App.DraftRoute = Ember.Route.extend({
 var attr = DS.attr;
 
 App.Project = DS.Model.extend({
-  property_id: attr('number'),
   title: attr('string'),
   description: attr('string'),
   body: attr('string'),
+  submittedOn: attr('number'),
   saved: attr('boolean'),
   isCompleted: DS.attr('boolean')
   
@@ -82,14 +91,14 @@ App.NewController = Ember.ObjectController.extend({
         var body = $('#body').val();
         var submittedOn = new Date();
         var store = this.get('store');
-        var project = store.createRecord('project',{ // I think that "store" may be messing me up here
+        var project = this.store.createRecord('project',{ // I think that "store" may be messing me up here
             title : title,
             description : description,
             body : body,
             submittedOn : submittedOn
         });
         project.save();
-        this.transitionToRoute('draft');
+        this.transitionToRoute('index');
     }
  }
 });
@@ -106,6 +115,17 @@ App.DraftController = Ember.ObjectController.extend({
 
 		doneEditing: function(){
 			this.set('isEditing', false);
+		},
+
+		completeDraft: function(){
+			var project = this.get('model');
+			project.set('isComplete', true);
+		},
+
+		removeProject: function() {
+		    var project = this.get('model');
+		    project.deleteRecord();
+		    project.save();
 		}
 	}
 })
